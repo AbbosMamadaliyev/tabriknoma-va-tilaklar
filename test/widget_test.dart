37bbb80ac7dd12aa7ecd227567e64216ab2da261
Abbos Mamadaliyev <abbbos166@gmail.com>
2022-05-29 11:53:53 +0500

    xml error fixed

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:tabriklar/main.dart';
+import 'package:tabriklar/my_app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
